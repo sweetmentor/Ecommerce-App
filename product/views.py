fc19f43481fda8e73b4276585b0dc13ddbb4d5a8
Stephen Afolabi <stephenafolabi@gmail.com>
2018-06-26 13:40:53 +0000

    updated views.py

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import HttpResponse
-# Create your views here.
+# Create your views here -- products.
 def get_products(request):
     products = Product.objects.all()
     return render(request, "products/products.html", {'products': products})
